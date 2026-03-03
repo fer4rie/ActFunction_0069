6bec654eca91c9077a54332c7f49fb650a8f84d6
fer4rie <firapuspita2106@gmail.com>
2026-03-03 16:21:11 +0700

    1

--- Praktikumpersegipanjang.cpp
+++ Praktikumpersegipanjang.cpp
@@ -4,3 +4,31 @@ using namespace std;
 //Deklarasi Global
     float  p, l;   
 
+//implementasi prosedur dan fungsi
+void input(){
+    cout << "Masukkan Panjang: ";
+    cin >> p;
+    cout << "Masukkan Lebar: ";
+    cin >> l;
+}
+
+float LuasPersegi(){
+    return p*l;
+}
+
+int Jumlah(int a, int b, int c){
+    return a + b + c;
+}
+
+void output(){
+    cout << "Hasilnya : " << LuasPersegi() << endl;
+}
+
+int main()
+{//start
+    input();
+    output();
+    cout << "Hasil Penjumlahan = " << Jumlah(8,6,9) << endl;
+}//selesai
+
+//ee
